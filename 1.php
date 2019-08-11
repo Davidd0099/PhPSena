cc3458c3d82fb1893b2fb9d9d557f78efa92572d
Davidd <46269969+Davidd0099@users.noreply.github.com>
2019-08-11 10:58:21 -0500

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>php1</title>
+  </head>
+  <body>
+   <form action = "1.php" method = "post">
+   	<input
+   <form/>
+  </body>
+</html>
